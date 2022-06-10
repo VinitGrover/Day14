691e722427b4167d7c9d73f86d1516fd163f575a
VinitGrover <vinitgrover255@gmail.com>
2022-06-10 18:42:33 +0530

    Add uc1

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
-public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Data Structure and Algorithm program ");
+public class LinkedList<K>{
+
+    Node<K> head, tail;
+    public void push(K key){
+        Node<K> newNode = new Node<>(key);
+        if (head != null) {
+            head.next = newNode;
+        }
+        head = newNode;
     }
+
+
+
+
 }
